3e6dd21665ab1ac75c01925fc2a59d70384e96e8
MBSinterns000 <sidish96@gmail.com>
2022-11-21 00:57:04 -0500

    flashlight on

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -5,3 +5,14 @@ window.addEventListener('scroll', function(e) {
   logo.setAttribute('transform',`translate(0 ${html.scrollTop / 10 + 5})`);
   clip.setAttribute('transform',`translate(0 -${html.scrollTop / 10 + 5})`);
 });
+
+function update(e){
+  var x = e.clientX || e.touches[0].clientX
+  var y = e.clientY || e.touches[0].clientY
+
+  document.documentElement.style.setProperty('--cursorX', x + 'px')
+  document.documentElement.style.setProperty('--cursorY', y + 'px')
+}
+
+document.addEventListener('mousemove',update)
+document.addEventListener('touchmove',update)
